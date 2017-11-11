58aaa5deb45d42b82f0f99b9e2600a0ae0a9e59c
Saikrishna Rao <krish98.sai@gmail.com>
2017-11-11 03:04:07 -0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_for :admins
   root to: 'home#index'
 
-  Rails.application.routes.draw do
-      devise_for :users, controllers: {
-        registrations: 'users/registrations'
-      }
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
