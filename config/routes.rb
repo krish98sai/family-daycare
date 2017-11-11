82afb41d838c96d0bff1ce995699454eaa0e01fb
Saikrishna Rao <krish98.sai@gmail.com>
2017-11-10 17:14:17 -0800

    Update user and admin model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: 'home#index'
 
-
+  Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
