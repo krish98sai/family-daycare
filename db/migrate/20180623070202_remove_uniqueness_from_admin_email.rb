d4180f0566899eb53228e516bbb4d7e23546d72c
Saikrishna Rao <krish98.sai@gmail.com>
2018-06-23 00:37:23 -0700

    config

--- db/migrate/20180623070202_remove_uniqueness_from_admin_email.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUniquenessFromAdminEmail < ActiveRecord::Migration[5.1]
-  def change
-    remove_index :admins, :email
-    add_index :admins, :email
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623070202) do
+ActiveRecord::Schema.define(version: 20171112080026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180623070202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_admins_on_email"
+    t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
   end
